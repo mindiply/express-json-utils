293581ddd8cedffcc7457fdff8b6317c32049556
Paolo Ferdinando Bongiovanni <paolo.bongiovanni@mindiply.com>
2016-10-12 10:29:10 +0100

    Initial version that passes the basic tests

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # express-json-utils
-Use standard result field in data and error objects returned from an express application
+Use standard result field in data and error objects returned from an 
+express application
+
+The API consists of 2 functions:
+
+* **sendJSONResult(res, data)** which sends an object of the format: { 
+  result : 'ok', data_field_1 : data_field_1_value, ...} back via res.send
+ 
+* **sendJSONError(res, err, msg)** which sends and object of the format: 
+  { result : 'error', msg : msg, err : err } via res.send
+  
+
+# License
+
+Apache 2.0
---
+++ test/api.js
@@ -0,0 +1,58 @@
+/**
+ * Created by bongio on 12/10/2016.
+ */
+"use strict";
+
+let expect = require('chai').expect
+let { sendJSONResult, sendJSONError }  = require('../lib')
+
+function createRes() {
+    let data = {}
+    return {
+        setHeader : (header, value) => {
+            data.headers = {[header] : value}
+        },
+        send : (send_data) => {
+            data.json = send_data
+        },
+        data
+    }
+}
+
+describe('api', () => {
+    describe('sendJSONResult()', () => {
+        it('should create the expected json object', () => {
+            let res = createRes()
+            let test_data = {
+                first : 'first',
+                second : 0,
+                third : [1,2,3],
+                fourth : {
+                    a : 1,
+                    b : 'Good lord',
+                    c : 21.23
+                }
+            }
+
+            sendJSONResult(res, test_data)
+            let expected_data = Object.assign({}, test_data, {result : 'ok'})
+            let parsed_data = JSON.parse(res.data.json)
+            expect(parsed_data).to.deep.equal(expected_data)
+            expect(res.data.headers).to.deep.equal({'Content-Type' : 'application/json'})
+        })
+    })
+
+    describe('sendJSONError()', () => {
+        it('should create the expected error object', () => {
+            let res = createRes()
+            let test_err = new Error('Test error')
+
+            sendJSONError(res, test_err,'do we have a test error?')
+            let expected_data = { err : test_err, msg : 'do we have a test error?', result : 'error' }
+            let parsed_data = JSON.parse(res.data.json)
+            expect(parsed_data).to.deep.equal(expected_data)
+            expect(res.data.headers).to.deep.equal({'Content-Type' : 'application/json'})
+        })
+    })
+
+})
